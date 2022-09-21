74ea1617e5a07d5073b20c57bbea63c68d25b0eb
JuezInquisidor <lic.jralarcon@hotmail.com>
2022-09-21 15:07:09 -0600

    Hola Mundo
    - se creo script "hola mundo" a main camera
    - se utilizaron las funciones de debug

---
+++ proyectoinicialebac/Assets/Script/HolaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Algo paso");
+        Debug.Log("Algo paso");
+        Debug.LogWarning("Algo salio moderadamente mal");
+        Debug.LogError("Algo slio muy mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
